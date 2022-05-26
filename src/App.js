58c4817a4a109210ec2cfa85ff15a3fb21976573
Emrecan Balgün <deathlygrave@gmail.com>
2022-05-26 20:48:16 +0300

    create filter and characters container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import Header from './container/Header';
-// import { Button,Row, Col } from 'react-bootstrap';
+import Filter from './container/Filter';
+import Characters from './container/Characters';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Container fluid>
+        <Row>
+          <Col className="col-4">
+            <Filter/>
+          </Col>
+          <Col className="col-8">
+            <Characters/>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
