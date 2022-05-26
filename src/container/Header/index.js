a60afd339f445f393d66aed910f93015e6d8cc64
Emrecan Balgün <deathlygrave@gmail.com>
2022-05-26 20:41:09 +0300

    update css and header

--- src/container/Header/index.js
+++ src/container/Header/index.js
@@ -6,12 +6,12 @@ function Header() {
   return (
     <Container fluid className="header">
         <Row>
-            <Col>
+            <Col className="p-3">
                 <span className="header__title fs-1">Wubba Lubba Dub Dub.</span>
             </Col>
         </Row>
         <Row>
-            <Col className="input-group input-group-lg input__size p-3">
+            <Col className="input-group input-group-lg input__size">
                 <span className="search input-group-text bg-white" id="inputGroup-sizing-lg"><FontAwesomeIcon className="p-2 text-warning fa-sm" icon={faSearch}/></span>
                 <input type="text" className="form-control fs-6" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" placeholder="Name, description, location …"/>
             </Col>
