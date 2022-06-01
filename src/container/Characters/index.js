1e40ddbfac9acf9b2cdaa6e84a7b114856038a0d
Emrecan Balgün <deathlygrave@gmail.com>
2022-06-01 21:10:54 +0300

    update characters index.js and add queries.js

--- src/container/Characters/index.js
+++ src/container/Characters/index.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import { GET_ALL_CHARACTERS } from './queries';
+import { useQuery } from "@apollo/client";
 
 function Characters() {
+  const { loading, error, data } = useQuery(GET_ALL_CHARACTERS);
+
   return (
     <div>Characters</div>
   )
