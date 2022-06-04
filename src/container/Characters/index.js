2b4c65da5dabfa25d7e497b939d052d3b0012a0f
Emrecan Balgün <deathlygrave@gmail.com>
2022-06-04 14:37:39 +0300

    refactor characters index.js and update css

--- src/container/Characters/index.js
+++ src/container/Characters/index.js
@@ -2,6 +2,7 @@ import { GET_ALL_CHARACTERS } from './queries';
 import { useQuery } from "@apollo/client";
 import Loading from '../../components/Loading';
 import Error from '../../components/Error';
+import { Container, Row, Col, Card, InputGroup, FormControl, DropdownButton, Dropdown  } from 'react-bootstrap';
 
 function Characters() {
   const { loading, error, data } = useQuery(GET_ALL_CHARACTERS);
@@ -15,18 +16,37 @@ function Characters() {
   }
 
   return (
-    <div>
-        {
-            data.characters.results.map((character) => (
-              <div key={character.id}>
-                <img src={character.image}/>
-                <p>{character.species}</p>
-                <p>{character.name}</p>
-                <p>{character.location.name}</p>
-              </div>
-            ))
-        }
-    </div>
+    <Container>
+      <Row>
+        {/* <InputGroup className="mb-3">
+          <DropdownButton
+            variant="outline-none"
+            title="16 hits per page"
+            id="input-group-dropdown-2"
+            align="end"
+          >
+            <Dropdown.Item active href="#">16 hits per page</Dropdown.Item>
+            <Dropdown.Item href="#">32 hits per page</Dropdown.Item>
+            <Dropdown.Item href="#">64 hits per page</Dropdown.Item>
+          </DropdownButton>
+        </InputGroup> */}
+        <hr className="my-4" />
+          {
+                data.characters.results.map((character) => (
+                  <Col className="col-3" key={character.id}>
+                    <Card className="border-0">
+                      <Card.Img variant="top" src={character.image}/>
+                      <Card.Body className="text-start zeroPadding ">
+                        <Card.Text className="characterCategory">{character.species}</Card.Text>
+                        <Card.Text className="characterName fw-bold">{character.name}</Card.Text>
+                        <Card.Text className="characterLocation">{character.location.name}</Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))
+            }
+      </Row>
+    </Container>
   )
 }
 
