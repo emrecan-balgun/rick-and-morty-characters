4faff1b104839f0d3223993a03905549420343c1
Emrecan Balgün <deathlygrave@gmail.com>
2022-06-10 14:31:46 +0300

    add gender filter but one page

--- src/container/Filter/index.js
+++ src/container/Filter/index.js
@@ -17,7 +17,18 @@ function Filter() {
     return <Loading />;
   }
 
-  // console.log(data.characters.results);
+  console.log(data.characters.results);
+
+  let gendersArray = [];
+
+  for(const gender of data.characters.results) {
+    if(!gendersArray.includes(gender.gender)) {
+      gendersArray.push(gender.gender);
+    }
+  }
+
+  console.log(gendersArray);
+  
 
   return (
     <Container fluid>
@@ -29,24 +40,19 @@ function Filter() {
           </div>
         </Col>
         <hr className="my-3"/>
-        <Col className="text-start">  
+        {/* Gender Filter */}
+        <Col className="text-start">
           <span className="filtersTitle">gender</span>
           {
-              data.characters.results.map((character) => (
-                <label className="checkContainer" key={character.id}>
-                  {character.gender}
-                  <input type="checkbox"/>
-                  {/* <Badge bg="light" text="dark">100</Badge> */}
-                  <span className="checkmark"></span>
-                </label>
-              ))
+              // data.characters.results.map((character) => (
+              //    <label className="checkContainer" key={character.id}>
+              //       {character.gender}
+              //       <input type="checkbox"/>
+              //       {/* <Badge bg="light" text="dark">100</Badge> */}
+              //       <span className="checkmark"></span>
+              //     </label>
+              // ))
           }
-            {/* <label className="checkContainer">
-              One
-              <input type="checkbox"/>
-              <Badge bg="light" text="dark">100</Badge>
-              <span className="checkmark"></span>
-            </label> */}
         </Col>
       </Row>
     </Container>
