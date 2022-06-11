a5ba17d141c8db064498074d30cf757f796a70e7
Emrecan Balgün <deathlygrave@gmail.com>
2022-06-11 18:36:06 +0300

    add gender value, location value, species value arrays for filter

--- src/app/rickAndMortySlice.js
+++ src/app/rickAndMortySlice.js
@@ -7,6 +7,9 @@ export const rickAndMortySlice = createSlice({
       perPage: 8,
       checked: false,
       searchValue: '',
+      genderValue: [],
+      speciesValue: [],
+      locationValue: [],
   },
   reducers: {
     changePageNumber(state, action) {
@@ -21,15 +24,35 @@ export const rickAndMortySlice = createSlice({
     changeSearchValue(state, action) {
         state.searchValue = action.payload
     },
-  },
-})
+    addGenderValue(state, action) {
+        state.genderValue.push(action.payload);
+    },
+    removeGenderValue(state, action) {
+        state.genderValue.splice(state.genderValue.indexOf(action.payload), 1);
+    },
+    addSpeciesValue(state, action) {
+        state.speciesValue.push(action.payload);
+    },
+    removeSpeciesValue(state, action) {
+        state.speciesValue.splice(state.speciesValue.indexOf(action.payload), 1);
+    },
+    addLocationValue(state, action) {
+        state.locationValue.push(action.payload);
+    },
+    removeLocationValue(state, action) {
+        state.locationValue.splice(state.locationValue.indexOf(action.payload), 1);
+    },
+}})
 
 // Action creators are generated for each case reducer function
-export const { changePageNumber, changePerPage, changeChecked, changeSearchValue } = rickAndMortySlice.actions;
+export const { changePageNumber, changePerPage, changeChecked, changeSearchValue, addGenderValue, removeGenderValue, addSpeciesValue, removeSpeciesValue, addLocationValue, removeLocationValue } = rickAndMortySlice.actions;
 
 export const pageNumber = (state) => state.rickAndMorty.pageNumber;
 export const perPage = (state) => state.rickAndMorty.perPage;
 export const checked = (state) => state.rickAndMorty.checked;
 export const searchValue = (state) => state.rickAndMorty.searchValue;
+export const genderValue = (state) => state.rickAndMorty.genderValue;
+export const speciesValue = (state) => state.rickAndMorty.speciesValue;
+export const locationValue = (state) => state.rickAndMorty.locationValue;
 
 export default rickAndMortySlice.reducer;
